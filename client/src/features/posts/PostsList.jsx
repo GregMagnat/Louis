5fc92c933f4c65be14b7a04a01fe383bcd099e45
100PBaguuette <magnat.gregory@gmail.com>
2024-02-27 11:14:01 +0100

    Added link to titles

--- client/src/features/posts/PostsList.jsx
+++ client/src/features/posts/PostsList.jsx
@@ -29,7 +29,11 @@ function PostsList() {
         <div>
             {posts.map((post) => (
                 <div key={post.id} className="post-container">
-                    <h2>{post.title}</h2>
+                    <h2>
+                        <Link to ={`/posts/${post.id}`} className="post-title">
+                        {post.title}
+                        </Link>
+                    </h2>
                     <p>{post.body}</p>
                 </div>
             ))}
